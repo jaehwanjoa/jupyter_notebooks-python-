80aada624afc14434c107f1ab24b206d78614a3a
jaehwanjoa <90813478+jaehwanjoa@users.noreply.github.com>
2024-01-23 18:37:56 +0900

    Update file_scan.py

--- file_scan.py
+++ file_scan.py
@@ -8,7 +8,7 @@ import json
 file = input('Input IoC Filehash(MD5) Entity: ')
 
 # results OTX Threat Intelligence Entity
-otx = OTXv2('2c5cf84144f2052145bee430b136da40757637989a7c6cfddb08e059377d5cad')
+otx = OTXv2('input OTX key')
 indicators1 = otx.get_indicator_details_full(IndicatorTypes.FILE_HASH_MD5, file)
 print('===============================OTX TI Detection===============================')
 print('fileinfo: ', pd.DataFrame(indicators1['analysis']['analysis']['info']))
@@ -18,7 +18,7 @@ print('related_pulse: ', pd.DataFrame(indicators1['general']['pulse_info']['rela
 url = "https://www.virustotal.com/api/v3/files/%s" % (file)
 headers = {
     'accept' : 'application/json',
-    'x-apikey' : 'e37f2e31aed5ea0cd706bc48be41b8c28fcdceace4d910b57b82be3ce601501b'
+    'x-apikey' : 'Input VT API Key'
 }
 req = urllib.request.Request(url, headers=headers)
 res = urllib.request.urlopen(req)
